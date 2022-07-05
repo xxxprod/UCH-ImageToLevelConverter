be734a844ce0c83f1eb0f35f0ee58fb10b47ddb2
xxxprod <tgr@lookat.at>
2022-07-05 11:53:53 +0200

    some cleanups and added a new icon for the optimizer

--- Views/ImageSelectorView.xaml.cs
+++ Views/ImageSelectorView.xaml.cs
@@ -1,16 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using System.Windows.Input;
 
 namespace UCH_ImageToLevelConverter.Views
 {
     public partial class ImageSelectorView
     {
-        private readonly HashSet<string> _numericKeyCodes = Enumerable.Range(0, 9)
-            .SelectMany(i => new[] { $"D{i}", $"NumPad{i}" })
-            .ToHashSet();
-
         private readonly Regex _filterNumericCharacterRegex = new("[^0-9]+", RegexOptions.Compiled);
 
         public ImageSelectorView() => InitializeComponent();
